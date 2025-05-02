a38ae8ed8f846e3e2a302dec9beaa4251e1026aa
janfrel <formaranjanfrel@gmail.com>
2025-05-02 16:05:38 +0800

    Update submodule content

--- banking_test.py
+++ banking_test.py
@@ -28,9 +28,9 @@ class TestAuthentication(unittest.TestCase):
             account = authenticate("12345", "9999")
             self.assertIsNone(account)
 
-    def test_invalid_account(self):
+    def test_invalid_account_format():
         account = authenticate("00000", "1111")  # Invalid account number
-        self.assertIsNone(account)
+        format.assertIsNone(account)
 
     def test_deposit_positive_amount_php(self):
         account = accounts["12345"]
